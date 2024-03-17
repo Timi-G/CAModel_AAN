2f7018239d2930890f48f469c77c9618d0366e9a
Timi-G <56408619+Timi-G@users.noreply.github.com>
2024-03-17 03:24:49 +0100

    Update run_cam_pont.py
    updated transit time

--- cam_potential_field/run_cam_pont.py
+++ cam_potential_field/run_cam_pont.py
@@ -20,24 +20,30 @@ get list of parameters on randomly generated a_crafts using rand_acraft_info()
 
 get aggregate positions of flight with f1.agg_pos,f2.agg_pos...
 get flight transition time with f1.avg_tnstime,f2.avg_tnstime...
+get total transit time in simulation with tma.transit_time
+get average transit time in simulation with tma.avg_transit_time
 get dens of resultant field with tma.dens
-get velocity with tma.vel and tma.avg_vel
+get velocity with tma.vel, tma.avg_vel_a and tma.avg_vel_b
 get flow of a given number of aircraft within specified time steps using cal_flow(t_steps,flights) it takes an optional func. tma 
 
 density is no. of flights/area of tma
 velocity is no. of movements throughout the simulation
-average velocity is no. of aircraft movement/total time step
+average velocity A is no. of aircraft movement/total time step
+average velocity B is no. of aircraft movement/total no. of aircraft
 
 show_vis_clip variable is used to set how many sims to run at a given time and which sim should have visualizations
 '''
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    t_steps = 5
     clip_no = 0
     grid_size = [20, 25]
-    show_vis_clip = [True, False, False]
+    show_vis_clip = [False]
     # try the convention:
     # show_vis_clip = [True] * 3 + [True] * 5 + [False] * 2
+    total_flows=[]
+    all_avg_transit_time=[]
 
     # declare variables for aircraft generation during repetition of simulation
     no_rand_aircrafts=[3, 2, 1, 2]
@@ -48,6 +54,8 @@ if __name__ == '__main__':
     for n,(nra,nas,ms,mp) in enumerate(zip(no_rand_aircrafts,no_aircrafts_from_sides,max_size,max_pot),0):
         # define a collector for desired quantities here
         # e.g. flows=[]
+        flows=[]
+        avg_transit_time=[]
         for no, show_single_clip in enumerate(show_vis_clip, 1):
             '''clip_no is index of video visualization'''
             clip_no=(n*len(show_vis_clip))+no
@@ -55,19 +63,16 @@ if __name__ == '__main__':
 
             tma = TMA(row=6,column=6,max_pot=50,grid_size=grid_size)
 
-            # single aircraft object creation still works as shown in f1,f2,f3,f4
             # f1 = Aircraft(row=5,column=9,max_pot=9,grid_size=grid_size,size=1,plt_color='b',null_pont=True)
             # f2 = Aircraft(row=3,column=5, max_pot=10, grid_size=grid_size, size=1,plt_color='r')
             # f3 = Aircraft(row=8,column=14, max_pot=7, grid_size=grid_size, size=2,plt_color='g')
             # f4 = Aircraft(row=10, column=9, max_pot=5, grid_size=grid_size, size=2, plt_color='w')
-            
-            # fr1 and fr2 are examples of creating multiple aircrafts at once
+            #
             # fr1 = multiple_aircrafts(rand_aircrafts=50,max_pot=5,grid_size=grid_size,max_size=2,plt_colors=['c','r','m','y'],null_pont=False)
             # fr2 = multiple_aircrafts(max_pot=5, grid_size=grid_size, max_size=2, plt_colors=['c', 'r', 'm', 'y'],
             #                         start_sides={'up':3,'down':1,'left':2,'right':1}, null_pont=False)
 
             # dynamic declaration of aircrafts for repetitive simulations only applies to variables fr3 & fr4
-            # this convention can be followed to define other multiple aircrafts variables
             fr3 = multiple_aircrafts(rand_aircrafts=nra,max_size=ms,max_pot=mp,grid_size=grid_size,plt_colors=['w','g','m','y'],null_pont=False)
             fr4 = multiple_aircrafts(max_pot=mp, grid_size=grid_size, max_size=ms, plt_colors=['b', 'r', 'm', 'g'],
                                     start_sides={'up':nas[0],'down':nas[1],'left':nas[2],'right':nas[3]}, null_pont=False)
@@ -84,10 +89,17 @@ if __name__ == '__main__':
             stat_obstructions = [sob1]
             mov_obstructions = [mob1]
 
-            simulate(flights,waypoints,stat_obstructions,mov_obstructions,field,show_single_clip,clip_no,total_tstep=10)
+            simulate(flights,waypoints,stat_obstructions,mov_obstructions,field,show_single_clip,clip_no,total_tstep=t_steps)
 
             # put functions to calculate any desired quantities here
             # e.g. flows+=[cal_flow(15,fr1)]
+            flows += [cal_flow(t_steps, flights)]
+
+            # get transit time for repetitions
+            avg_transit_time+=[field[0].avg_transit_time]
 
         # find average of quantities here
         # e.g. avg_flow=sum(flows)/len(flows)
+        total_flows+=[flows]
+        all_avg_transit_time+=[avg_transit_time]
+    # avg_flow = sum(flows) / len(flows)
