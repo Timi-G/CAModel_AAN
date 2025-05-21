7981033e1f44f446b846a5db369affc7f1165c2d
Timi-G <56408619+Timi-G@users.noreply.github.com>
2025-05-21 23:23:17 +0100

    Update run_cam_airnav.py
    Added path & path_prob parameter to object initialization

--- aan_models/run_cam_airnav.py
+++ aan_models/run_cam_airnav.py
@@ -38,24 +38,31 @@ if __name__ == '__main__':
 
     w1 = Waypoint(pos=[-13,20], size=2)
     w2 = Waypoint(pos=[10,20], size=2)
-    w3 = Waypoint(pos=[15,5], size=1)
+    w3 = Waypoint(pos=[25,5], size=1)
     w4 = Waypoint(pos=[20,-8], size=3)
     w5 = Waypoint(pos=[-10,-8], size=1)
 
-    f1 = Flight(dept=[-7,3], dest=dest, t_down=t_down, waypoints=[w1,w2,w3], size=2)
-    f2 = Flight(dept=[25,25], dest=dest, t_down=t_down, waypoints=[w3,w2,w1], size=1)
-    f3 = Flight(dept=[12,0], dest=dest, t_down=t_down, waypoints=[w4,w5,w2], size=2) #[3,8]
-    f4 = Flight(dept=[13,9], dest=dest, t_down=t_down, waypoints=[w5,w2,w3,w4], size=1)
-    f5 = Flight(dept=[5,5], dest=dest, t_down=t_down, waypoints=[w4,w5,w3], size=1) #[10,3]
-    f6 = Flight(dept=[1,1], dest=dest, t_down=t_down, waypoints=[w1,w2,w3, w5], size=1) #[15,2]
+    path1 = [w1,w2,w3]
+    path2 = [w3,w2,w1]
+    path3 = [w4,w5,w2]
+    path4 = [w5,w2,w3,w4]
+    path5 = [w4,w5,w3]
+    path6 = [w1,w2,w3, w5]
 
-    fr1 = create_flights(north=3, south=2, east=0, west=0, tma=tma, dest=dest, t_down=t_down, waypoints=[w1,w2,w3], size=1, spread=1) #[5, 1]
-    fr2 = create_flights(north=1, south=0, east=2, west=4, tma=tma, dest=dest, t_down=t_down, waypoints=[w2,w4,w3,w5], size=2, spread=1) #[7, 15]
-    fr3 = create_flights(north=2, south=4, east=1, west=0, tma=tma, dest=dest, t_down=t_down, waypoints=[w2,w1,w3,w4], size=1, spread=1) #[17, 20]
+    f1 = Flight(dept=[-7,3], dest=dest, t_down=t_down, paths=[path1,path3,path4], path_prob=[3,5,2], size=2)
+    f2 = Flight(dept=[25,25], dest=dest, t_down=t_down, paths=[path5,path1,path2], path_prob=[3,5,2], size=1)
+    f3 = Flight(dept=[12,0], dest=dest, t_down=t_down, paths=[path2,path3], path_prob=[3,7,], size=2) #[3,8]
+    f4 = Flight(dept=[13,9], dest=dest, t_down=t_down, paths=[path6,path1,path2], path_prob=[3,5,2], size=1)
+    f5 = Flight(dept=[5,5], dest=dest, t_down=t_down, paths=[path4], path_prob=[10], size=1) #[10,3]
+    f6 = Flight(dept=[1,1], dest=dest, t_down=t_down, paths=[path5,path1,path6], path_prob=[3,1,2,4], size=1) #[15,2]
+
+    fr1 = create_flights(north=5, south=15, east=6, west=10, tma=tma, dest=dest, t_down=t_down, paths=[path2,path3,path5,path4], path_prob=[2,3,1,4], size=1, spread=1) #[5, 1]
+    fr2 = create_flights(north=1, south=0, east=2, west=4, tma=tma, dest=dest, t_down=t_down, paths=[path6,path1], path_prob=[3,7], size=2, spread=1) #[7, 15]
+    fr3 = create_flights(north=2, south=4, east=1, west=0, tma=tma, dest=dest, t_down=t_down, paths=[path1,path2,path3], path_prob=[3,5,2], size=1, spread=1) #[17, 20]
 
     ob1 = Obstruction(pos=[0,10], size=2)
     ob2 = Obstruction(pos=[5,10], size=3)
-    ob3 = Obstruction(pos=[15,10], size=3)
+    ob3 = Obstruction(pos=[25,10], size=3)
     ob4 = Obstruction(pos=[-10,-4], size=2)
     ob5 = Obstruction(pos=[0,-4], size=2)
     ob6 = Obstruction(pos=[12,-4], size=1)
@@ -70,6 +77,6 @@ if __name__ == '__main__':
 
 
 # to run simulation
-    simulate(tma,flights,waypoints,obstructions,points,no_flyzone_size,tdown_dest_path_size,total_tsteps=80)
+    simulate(tma,flights,waypoints,obstructions,points,no_flyzone_size,tdown_dest_path_size,total_tsteps=350)
 
-    #display_results(tma,flights,waypoints)
+    # display_results(tma,flights,waypoints)
